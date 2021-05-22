141562a3a63e464e2dc15e8dab8ae7e594ddf2c2
Eranga <erangakm@gmail.com>
2021-05-22 01:12:56 -0700

    more configs

--- configure-git.sh
+++ configure-git.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 email="erangakm@gmail.com"
+name="Eranga Manchanayake"
 
 ssh-keygen -t ed25519 -C $email
 
@@ -8,3 +9,4 @@ ssh-add
 ssh-add -L
 
 git config --global user.email $email
+git config --global user.name $name
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo -v
+. install-homebrew.sh
